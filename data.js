206830cb755f305faf6d41d861a67dbb0893358a
Hamza Bari <barihamza73@gmail.com>
2026-05-30 21:58:25 +0100

    cretae quiz html and data js

---
+++ data.js
@@ -0,0 +1,44 @@
+const quizData = {
+    "HTML": {
+        questions: [
+            {
+                question: "What does HTML stand for?",
+                options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Tool Multi Language"],
+                answer: "Hyper Text Markup Language"
+            },
+            {
+                question: "Choose the correct HTML element for the largest heading:",
+                options: ["<heading>", "<h6>", "<head>", "<h1>"],
+                answer: "<h1>"
+            },
+            {
+                question: "Which character is used to indicate an end tag?",
+                options: ["*", "/", "<", "^"],
+                answer: "/"
+            }
+        ]
+    },
+    "CSS": {
+        questions: [
+            {
+                question: "What does CSS stand for?",
+                options: ["Computer Style Sheets", "Cascading Style Sheets", "Colorful Style Sheets", "Creative Style Sheets"],
+                answer: "Cascading Style Sheets"
+            },
+            {
+                question: "Which property is used to change the background color?",
+                options: ["color", "bgcolor", "background-color", "canvas-color"],
+                answer: "background-color"
+            }
+        ]
+    },
+    "PYTHON&ALGO": {
+        questions: [
+            {
+                question: "How do you create a function in Python?",
+                options: ["function my_function():", "def my_function():", "create my_function():", "def function my_function():"],
+                answer: "def my_function():"
+            }
+        ]
+    }
+};
