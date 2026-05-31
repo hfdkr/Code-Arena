8c539fd2ddc74b7f154dd56bb863dad4d701e08f
Hassan-Akbad <akbad091@gmail.com>
2026-05-31 17:56:39 +0100

    feat: implement quiz logic and timer functionality in quiz.js; update score and timer display in quiz.html

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function escapeHTML(str) {
+    return str
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;");
+}
+
+let currentQuestionIndex = 0;
+let selectedCategory = sessionStorage.getItem('quizCategory') || 'HTML';
+let selectedDifficulty = sessionStorage.getItem('quizDifficulty') || 'easy';
+let score = 0;
+let answers = [];
+let timerInterval;
+let timeRemaining = 30;
+let answered = false;
+
+let quiz;
+let questions;
